4f2d6eb29f0cd2562c006a665e3d9e08dd538bbc
ExiNni <sangsa02@naver.com>
2023-08-31 18:59:12 +0900

    practice4

---
+++ Practice/src/com/kh/practice/func/Practice1.java
@@ -0,0 +1,23 @@
+package com.kh.practice.func;
+
+import java.util.Scanner;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요: ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			System.out.println(i);
+		}
+		if(num <= 0) {
+			System.out.println("1이상의 숫자를 입력해주세요.");
+		}
+		
+	}
+
+}
